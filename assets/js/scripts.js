90c9f9b4af6dbeceea552508d65fb50bcae54acf
ayelenst <ayelenst@gmail.com>
2017-07-14 17:54:19 -0300

    adding closing window
    adding closing window

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -320,11 +320,12 @@ Version      : 1.0
 			captionText.innerHTML = this.alt;
 		}
 
+		
 		// Get the <span> element that closes the modal
 		var span = document.getElementsByClassName("close")[0];
 
 		// When the user clicks on <span> (x), close the modal
-		span.onclick = function() { 
+		modal.onclick = function() { 
 		  modal.style.display = "none";
 		}
 		
